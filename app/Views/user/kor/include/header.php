c3d2cc8a072019e6f2247443d4a094ebe6eadf55
timothy99 <joydisk@gmail.com>
2026-02-25 14:47:42 +0900

    노토산스 적용

--- app/Views/user/kor/include/header.php
+++ app/Views/user/kor/include/header.php
@@ -7,6 +7,8 @@
 
         <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
 
+        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;500;700&display=swap" rel="stylesheet"><!-- Noto Sans KR -->
+
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
 
